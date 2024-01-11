0da35f1fb5e4e5f07ade92ec1aed4406e1a08a60
Teo Vortex <vortexteo@gmail.com>
2024-01-11 13:48:08 -0800

    Traceability Lab

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+  
+// 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '4a73ac1651154d14aef9ec6c1b3e247c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Application Started Successfully')
+
+app.use(express.json())
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+           res.status(400).send('You must enter a name.')
+       } else {
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
